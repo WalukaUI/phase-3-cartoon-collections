6b27c875d1796e9639ffc13a2d73d7118cad815b
WalukaUI <cwaluka@yahoo.com>
2021-07-04 00:34:25 -0700

    completed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (array)# code an argument here
   # Your code here
+array.each_with_index do |word,index|
+  puts "#{index+1} #{word}"
+end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (calls)# code an argument here
   # Your code here
+calls.map do |word|
+  if(word.include?"!")
+     puts word.slice(0, word.length() - 1).downcase
+  else
+     "#{word.capitalize}!"
+  end
+ 
+end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(call)# code an argument here
   # Your code here
+  call.any? do |word|
+    word.size>4? true : false
+  end
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(name)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  name.find do |cheese|
+    cheese_types.include? (cheese)
+  end
 end
